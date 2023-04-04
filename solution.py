5b8715d6b80eb8b2198efcf26b76d72fb931fc91
MolodykhV <129886700+MolodykhV@users.noreply.github.com>
2023-04-04 20:26:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 238786813 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    x = x / 39**2
     return x.mean() # Ваш ответ
